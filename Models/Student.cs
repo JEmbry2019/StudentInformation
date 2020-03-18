d80cd271f891680210551b9e0c4efe9fad4db542
JEmbry2019 <jmsembry@gmail.com>
2020-03-18 12:54:55 -0400

    Phone Number

--- Models/Student.cs
+++ Models/Student.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
+
 namespace StudentInformation.Models
 {
     public class Student
@@ -11,14 +12,26 @@ namespace StudentInformation.Models
         public string FirstMidName { get; set; }
 
         
-        //[DataType(DataType.EmailAddress)]
-        //public object EmailAddress;
-        
-
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
-        
+
+        //[DataType(DataType.EmailAddress)]
+        //public object EmailAddress;
+        //public void Add (string addresses);
+        //public MailAddressCollection ();
+
+            private string phoneNumber;
+
+            public string GetPhoneNumber()
+            {
+                return phoneNumber;
+            }
+
+            public void SetPhoneNumber(string value)
+            {
+                phoneNumber = value;
+            }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
